d0bdf35f4ec230c6868bb442a02fee265729135b
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-08-27 18:38:25 +0100

    Add Anim module. Just some timing curves.

---
+++ 2048/src/anim.ml
@@ -0,0 +1,26 @@
+
+
+open Gg
+
+(* Ease in out, adapted from http://www.flong.com/texts/code/shapers_exp/ *)
+
+let d_eps = 0.00001
+let d_min_a = 0.0 +. d_eps
+let d_max_a = 1.0 -. d_eps
+
+let double_exp_sigmoid a t =
+  let a = Float.clamp ~min:d_min_a ~max:d_max_a a in
+  let exp = 1.0 /. (1.0 -. a) in
+  if t <= 0. then 0. else
+  if t >= 1. then 1. else
+  if t <= 0.5 then 0.5 *. ((2.0 *. t) ** exp) else
+  1.0 -. 0.5 *. ((2.0 *. (1.0 -. t)) ** exp)
+
+let ease_in_out ?(a = 0.426) t = double_exp_sigmoid a t
+
+(* Triangle around 0.5 *)
+
+let tri t =
+  if t <= 0. then 0. else
+  if t >= 1. then 0. else
+  1. -. abs_float (2. *. t -. 1.)
