d767443c4dca4ca0365154dfe9ccb0a57ac445ca
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-08-28 10:40:41 +0100

    Rename Main.user_moves to Main.user_move.
    Don't use plural on an event that occurs with a non
    plural value.

--- 2048/src/main.ml
+++ 2048/src/main.ml
@@ -5,7 +5,7 @@ open Useri
 
 let log fmt = Format.printf (fmt ^^ "@.")
 
-let user_moves : G2048.move event =
+let user_move : G2048.move event =
   let u = E.stamp (Key.up (`Arrow `Up))    G2048.U in
   let d = E.stamp (Key.up (`Arrow `Down))  G2048.D in
   let l = E.stamp (Key.up (`Arrow `Left))  G2048.L in
@@ -17,7 +17,7 @@ let board : G2048.board signal =
     if G2048.is_board_full board then G2048.create_board () else
     G2048.game_move m board
   in
-  S.accum (E.map move user_moves) (G2048.create_board ())
+  S.accum (E.map move user_move) (G2048.create_board ())
 
 let render_board_img r _ (size, img) =
   let renderable = `Image (size, Box2.unit, img) in
@@ -29,7 +29,7 @@ let setup () =
   let scene = S.Pair.pair Surface.size board_img in
   let c = Useri_jsoo.Surface.Handle.to_js (Surface.handle ()) in
   let r = Vgr.create (Vgr_htmlc.target ~resize:false c) `Other in
-  Surface.set_refresher user_moves;
+  Surface.set_refresher user_move;
   App.sink_event (S.sample (render_board_img r) Surface.refresh scene);
   ()
 
